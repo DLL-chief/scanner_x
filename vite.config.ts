6994e146657a5416db0bf97f1ef24dbcd96028c0
DLL-chief <d.a.strygin@gmail.com>
2026-06-19 20:29:41 +0300

    Update vite.config.ts for React and Worker support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   worker: {
-    format: 'es',
-  },
-  optimizeDeps: {
-    exclude: ['@huggingface/transformers'],
-  },
+    format: 'es'
+  }
 })
